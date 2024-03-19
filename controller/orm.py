aaa813cb2ca402d8a762e1c6e37a5b53cd62c1e8
Wang Xinyu <hi_wangxinyu@163.com>
2024-03-19 17:28:38 +0800

    更新实验拓扑visio

--- controller/orm.py
+++ controller/orm.py
@@ -1,6 +1,6 @@
-from typing import Generator, Optional
+from typing import Optional
 from enum import Enum
-from sqlmodel import Field, SQLModel, create_engine,Session
+from sqlmodel import Field, SQLModel, create_engine
 from datetime import datetime
 from pathlib import Path
 
@@ -13,20 +13,32 @@ class NodeType(Enum):
     SWITCH = "switch"
 
 class Node(SQLModel, table=True):
+    """
+    结点信息。包括终端和交换机。
+    此表是手动根据网络配置填写的。
+    """
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str 
     type: NodeType 
     ip:Optional[str] = Field(default=None,description="主机的ip地址, 由'.'分割的四个数，类似'1.1.1.1';交换机的此字段为空")
-    console_ip:str = Field(description="ssh(192.168.199.xxx)")
+    console_ip:str = Field(description="用于管理的内网ip地址和端口，例如192.168.199.3:22")
 
 class Link(SQLModel, table=True):
+    """
+    此表是手动根据配置的网络填写的。
+    连接有方向（因为int）。两个结点之间需要添加来回两个连接。
+    """
     id: Optional[int] = Field(default=None, primary_key=True)
     node_id_1:int
-    interface_id_1:int=Field(description="两个id按照字典序排序")
+    interface_id_1:int
     node_id_2:int
-    interface_id_2:int=Field(description="两个id按照字典序排序")
+    interface_id_2:int
 
 class LinkState(SQLModel, table=True):
+    """
+    由int程序写入的测试信息
+    schedule程序读取
+    """
     id: Optional[int] = Field(default=None, primary_key=True)
     link_id:int
     create_time:datetime
@@ -36,21 +48,33 @@ class LinkState(SQLModel, table=True):
 
 #网卡
 class Interface(SQLModel, table=True):
+    """
+    网卡信息，也包括网卡对应的bmv2端口号。
+    此表是手动根据网络配置填写的。
+    """
     id: Optional[int] = Field(default=None, primary_key=True)
     name:str
     node_id:int
-    bmv2_port:Optional[int] = Field(default=-1,description="交换机网卡对应的bmv2 port;主机网卡置为-1")
+    bmv2_port:Optional[int] = Field(default=-1,description="交换机网卡对应的bmv2 port;主机网卡置为-1或None")
 
-#测试任务；暂未确定，需要进一步讨论；
 class Task(SQLModel, table=True):
+    """
+    测试任务
+    此表暂未确定，需要进一步讨论；
+    """
     id: Optional[int] = Field(default=None, primary_key=True)
     src_host_id:int
     src_port:int
     dst_host_id:int
     dst_port:int
     enable:bool
+    message:str
 
 class TaskResult(SQLModel, table=True):
+    """
+    测试任务的结果
+    此表暂未确定，需要进一步讨论；
+    """
     id: Optional[int] = Field(default=None, primary_key=True)
     task_id:int
     create_time:datetime
@@ -61,10 +85,6 @@ class TaskResult(SQLModel, table=True):
 
 def _create_db_and_tables() -> None:
     SQLModel.metadata.create_all(engine)
-
-def get_session() -> Generator[Session,None,None]:
-    with Session(engine) as session:
-        yield session
 
 if __name__ == "__main__":
     _create_db_and_tables()
