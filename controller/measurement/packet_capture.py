fa932a5c9b8b04c6535aa48080c55932183c9502
jiumingaaa <10833710+jiumingaaa@user.noreply.gitee.com>
2024-05-29 11:49:34 +0800

    修复一些错误，可以捕获视频服务器端口号

--- controller/measurement/packet_capture.py
+++ controller/measurement/packet_capture.py
@@ -8,20 +8,24 @@ import re
 from concurrent.futures import ThreadPoolExecutor
 
 
-duration = 10  # 指定网卡监听时间，一般比视频时长稍大一些
-interface = 'eno1'  # 指定要监听的网络接口
+duration = 110  # 指定网卡监听时间，一般比视频时长稍大一些
+
+server_interface = 'eno2'  # 指定要监听的网络接口
+client_interface = 'eno2'
 
 # 客户端和服务器ip
-client_host = '192.168.199.170'
-server_host = '192.168.199.180'
+client_host = '10.170.170.2'
+server_host = '10.180.180.2'
+
+trans_server_host = '192.168.199.180'
 
 username = 'sinet'
 password = 'bjtungirc'
 yes = 'yes'
 
 # 180视频服务器端口（目前，之后可能会更改）
-quic_port = 8000
-webrtc_port = 8001
+quic_port = 8080
+webrtc_port = 8081
 https_port = 8002
 
 
@@ -32,14 +36,14 @@ client_path = "/home/sinet/project_measure/client.pcap"
 video_path = "/home/sinet/project_measure/webrtc/480p.mp4"
 
 
-def start_server_tcpdump(server_host, username, password, interface):
+def start_server_tcpdump(server_host, username, password, server_interface):
     sudo_pass = Responder(
         pattern=f'\[sudo\] password for {username}:',
         response=password + '\n'
     )
     
     with Connection(host=server_host, user=username, connect_kwargs={"password": password}) as conn:
-        conn.run(f"sudo timeout {duration} tcpdump -i {interface} src {server_host} and dst {client_host} -w {server_path}", pty=True, watchers=[sudo_pass], warn=True)
+        conn.run(f"sudo timeout {duration} tcpdump -i {server_interface} src {server_host} and dst {client_host} -w {server_path}", pty=True, watchers=[sudo_pass], warn=True)
 
 
 def start_client_tcpdump(host, username, password, interface):
@@ -53,27 +57,29 @@ def start_client_tcpdump(host, username, password, interface):
         response=yes + '\n'
     )
     scp_pass = Responder(
-        pattern=f'sinet\@{server_host}',
+        pattern=f'sinet\@{trans_server_host}',
         response=password + '\n'
     )
 
     with Connection(host=client_host, user=username, connect_kwargs={"password": password}) as conn:
-        conn.run(f"sudo timeout {duration} tcpdump -i {interface} src {server_host} and dst {client_host} -w client.pcap", pty=True, watchers=[sudo_pass], warn=True)
+        conn.run(f"sudo timeout {duration} tcpdump -i {client_interface} src {server_host} and dst {client_host} -w client.pcap", pty=True, watchers=[sudo_pass], warn=True)
         # 将抓到的包通过scp发送给180服务器
-        conn.run(f"scp /home/sinet/client.pcap {username}@{server_host}:{client_path}", pty=True, watchers=[connect_pass, scp_pass], warn=True)
+        conn.run(f"scp /home/sinet/client.pcap {username}@{trans_server_host}:{client_path}", pty=True, watchers=[connect_pass, scp_pass], warn=True)
 
 
 def get_port():
     sudo_pass = Responder(
         pattern=f'\[sudo\] password for {username}:',
+        # pattern=f'{username}@{trans_server_host}', 
         response=password + '\n'
     )
     
     with Connection(host=server_host, user=username, connect_kwargs={"password": password}) as conn:
-        result = conn.run(f"sudo timeout 10 tcpdump -i eno1 port {quic_port} or port {webrtc_port} or port {https_port} -n -vv | grep {server_host} | sed -n '1p'" , pty=True, watchers=[sudo_pass], warn=True)
+        result = conn.run(f"sudo timeout 20 tcpdump -i {server_interface} port {quic_port} or port {webrtc_port} or port {https_port} -n -vv | grep {server_host} | sed -n '1p'" , pty=True, watchers=[sudo_pass], warn=True)
         # 处理输出，获取服务器端口号
         output = result.stdout.strip()
-        match = re.search(r'(\b192\.168\.199\.180\b)\.(\d+)', output)
+        match = re.search(r'(\b10\.180\.180\.2\b)\.(\d+)', output)
+        # match = re.search(r'(\b192\.168\.199\.180\b)\.(\d+)', output)
 
         if match:
             ip_port = match.group(2)
@@ -87,15 +93,16 @@ def get_port():
 start_time = time.time()
 
 # 开启多线程
+
 with ThreadPoolExecutor() as executor:
     # 提交任务
-    t1 = executor.submit(start_client_tcpdump, client_host, username, password, interface)
-    t2 = executor.submit(start_server_tcpdump, server_host, username, password, interface)
+    t1 = executor.submit(start_client_tcpdump, client_host, username, password, client_interface)
+    t2 = executor.submit(start_server_tcpdump, server_host, username, password, server_interface)
     t3 = executor.submit(get_port)
 
     # 等待任务完成并获取端口号结果
     ip_port = int(t3.result())
-    
+
 
 # 对数据包进行处理
 # 服务端
@@ -129,13 +136,15 @@ information = {"tail_delay":tail_delay, "congestion_rate":congestion_rate, "reso
 
 print(information)
 
+
+
 # 将指标传输至数据库（目前数据库没有部署在服务器上）
 url = 'http://219.242.112.215:8000/Videosituation/add'
 
 # 要发送的数据
 data = information
 
-
+"""
 # 发送 POST 请求
 response = requests.post(url, json=data)
 
@@ -144,7 +153,7 @@ if response.status_code == 200:
     print("数据成功发送到数据库接口！")
 else:
     print("数据发送失败。")
-
+"""
 
 
 
--- controller/measurement/packet_handle.py
+++ controller/measurement/packet_handle.py
@@ -11,12 +11,13 @@ def extract_id_timestamp(file_path):
             eth = dpkt.ethernet.Ethernet(buf)
             if isinstance(eth.data, dpkt.ip.IP):
                 ip = eth.data
-                if isinstance(ip.data, dpkt.udp.UDP) or isinstance(ip.data, dpkt.tcp.TCP):
+                #if isinstance(ip.data, dpkt.udp.UDP) or isinstance(ip.data, dpkt.tcp.TCP):
+                if isinstance(ip.data, dpkt.udp.UDP):
                     packets[ip.id] = timestamp
     return packets
 
 
-# 测试尾时延和卡顿率
+# 测试尾时延(此处定义为最大时延)
 def calculate_tail_delay_and_congestion_rate(packets1, packets2):
     # 尾时延
     tail_delay = 0
@@ -29,14 +30,20 @@ def calculate_tail_delay_and_congestion_rate(packets1, packets2):
             tail_delay = max(tail_delay, abs(arrival_time2 - arrival_time1))
     if delay:
         average_value = sum(delay) / len(delay)
-        #print(delay)
 
-    # 求卡顿率（自定义）
+    
     values_greater_than_average = []
     for num in delay:
         if num > average_value * 1:
             values_greater_than_average.append(num)
+    
+    
     return round(tail_delay, 4), round(len(values_greater_than_average) / len(delay) * 100 if delay else 0, 3)
+
+    
+# 测试卡顿率（这里新定义卡顿率，就是求出每一个包的到达时间，一旦后一个包的到达时间超过前一个包的两倍，则计入卡顿数据包）
+def calculate_congestion_rate():
+    pass
 
 
 # 测试分辨率
